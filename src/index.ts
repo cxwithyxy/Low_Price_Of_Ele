9155c002d0cb4dfbe1f52731caf18d65418b49f8
chenxuan <523764720@qq.com>
2018-08-07 14:01:04 +0800

    找到命令行终端的编程方式

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import puppeteer = require("puppeteer");
 import Config = require("./Config");
+import Config = require("./utils/Live_Commandline");
 
-(async () => {
-    console.log(Config.launch_Config());
-    // const browser = await puppeteer.launch(Config.launch_Config());
-    // const page = await browser.newPage();
-    // page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
-    // await page.goto('https://ele.me');
-})();
+// (async () => {
+//     console.log(Config.launch_Config());
+//     const browser = await puppeteer.launch(Config.launch_Config());
+//     const page = await browser.newPage();
+//     page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+//     await page.goto('https://ele.me');
+// })();
+
