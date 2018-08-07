5aeced304233c0a322b4ad10067fd50397c5eaef
chenxuan <523764720@qq.com>
2018-08-07 16:52:23 +0800

    Live_Commandline: 增加多行模式

--- src/utils/Live_Commandline.ts
+++ src/utils/Live_Commandline.ts
@@ -18,6 +18,8 @@ class Live_Commandline extends Singleton {
         return this.instance;
     }
 
+    public typed_Word = "";
+
     constructor()
     {
         super();
@@ -29,13 +31,23 @@ class Live_Commandline extends Singleton {
     }
 
 
-    run(runtime_Object)
+    run(runtime_Object, mutiline=false)
     // runtime_Object 是一个对象，在命令行运行时，可对该对象进行操作。
+    // mutiline 为true时, 启动多行模式, 遇到 > 后才会执行代码.
     {
         process.stdin.setEncoding('utf8');
         process.stdin.resume();
         process.stdin.on('data', function(chunk) {
-            Live_Commandline.getInstance().runtime_In_Obj_JS_Code(runtime_Object, chunk);
+            if(!mutiline){
+                Live_Commandline.getInstance().runtime_In_Obj_JS_Code(runtime_Object, chunk);
+            }else{
+                if(chunk.substring(0,1) != ">"){
+                    Live_Commandline.getInstance().typed_Word += chunk;
+                }else{
+                    Live_Commandline.getInstance().runtime_In_Obj_JS_Code(runtime_Object, Live_Commandline.getInstance().typed_Word);
+                    Live_Commandline.getInstance().typed_Word = "";
+                }
+            }
         });
         console.log('Live_Commandline running');
     }
