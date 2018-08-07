418e5a139e0bf3338ac8d7ce63a7b92ed20dd0ca
chenxuan <523764720@qq.com>
2018-08-07 17:52:54 +0800

    初步完成位置定位,基于已有的位置配置文件

--- src/Config.ts
+++ src/Config.ts
@@ -7,5 +7,15 @@ class Config {
             devtools : true
         }
     }
+
+    static UA_Config()
+    {
+        return "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1";
+    }
+
+    static STORE_LS_Config()
+    {
+        return '{"userId":0,"location":{"geohash":"ws0eegnkxc87","latitude":23.132495,"longitude":113.376836,"timestamp":1533633899,"locationName":"乐天大厦(西北门)"},"cityId":4,"districtId":5623}';
+    }
 }
 export = Config;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
 import puppeteer = require("puppeteer");
 import Config = require("./Config");
 import Live_Commandline = require("./utils/Live_Commandline");
+import sleep = require("sleep-promise");
 
+var main_Object = {
+    log: console.log
+};
 
-Live_Commandline.getInstance().run({a:2});
-// (async () => {
-//     console.log(Config.launch_Config());
-//     const browser = await puppeteer.launch(Config.launch_Config());
-//     const page = await browser.newPage();
-//     page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
-//     await page.goto('https://ele.me');
-// })();
+(async () => {
+    var page, browser, frame;
+    main_Object["Config"] = Config;
+    main_Object["browser"] = browser = await puppeteer.launch(Config.launch_Config());
+    main_Object["page"] = page = await browser.newPage();
+    page.setUserAgent(Config.UA_Config());
+    await page.goto('https://ele.me');
+    await sleep(2000);
+    await page.evaluate((STORE)=>{
+        localStorage.setItem("STORE", STORE);
+    }, Config.STORE_LS_Config());
+    await page.goto('https://ele.me');
+    await sleep(2000);
+    // await page.$eval('header > div > div', el => el.click());
+    // await page.$$eval('.wrapper span', (els) => {
+    //     els.map((el) => {
+    //         if(el.innerText.indexOf("选择城市") != -1){
+    //             el.click();
+    //         }
+    //     });
+    //     console.log(els.length);
+    // });
+    // await sleep(2000);
+    // var search_Input = await page.$('input');
+    // await search_Input.press("Enter");
+    // await search_Input.type("广州市",{delay: 300});
 
+})();
+
+Live_Commandline.getInstance().run(main_Object, true);
