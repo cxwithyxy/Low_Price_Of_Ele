80b965a49642a2583d945958696ce14190859343
chenxuan <523764720@qq.com>
2018-08-07 13:32:31 +0800

    初次用es2017规范

---
+++ src/Config.ts
@@ -0,0 +1,11 @@
+class Config {
+    
+    static launch_Config()
+    {
+        return {
+            executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+            devtools : true
+        }
+    }
+}
+export = Config;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import puppeteer = require("puppeteer");
+import Config = require("./Config");
+
+(async () => {
+    console.log(Config.launch_Config());
+    // const browser = await puppeteer.launch(Config.launch_Config());
+    // const page = await browser.newPage();
+    // page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+    // await page.goto('https://ele.me');
+})();
