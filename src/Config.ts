d389d4abbe6976cedb1c563dad5f331244e6b1d7
chenxuan <523764720@qq.com>
2018-08-08 22:57:02 +0800

    Config: 优化配置读取方式, 现在部分个人信息会被储存在.ele.conf.json文件中, 避免后续开发泄露个人信息

--- src/Config.ts
+++ src/Config.ts
@@ -1,10 +1,47 @@
-class Config {
+import Conf = require("Conf");
+import Singleton = require("./base/Singleton");
+
+class Config extends Singleton{
     
+    static instance : Config;
+
+    public static getInstance() : Config
+    {
+        if(!this.instance)
+        {
+            this.instance = new Config();
+        }
+        return this.instance;
+    }
+
+    private conf_Driver: Conf;
+
+    constructor()
+    {
+        super();
+        this.conf_Driver = new Conf({
+            configName: ".ele.conf",
+            cwd: process.cwd()
+        });
+    }
+
+    get_Conf_Driver()
+    {
+        return this.conf_Driver;
+    }
+
+    get_Conf_Value(_key:String)
+    {
+        let return_V = Config.getInstance().get_Conf_Driver().get(_key);
+        return return_V;
+    }
+
     static launch_Config()
     {
         return {
             executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-            devtools : true
+            devtools : true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         }
     }
 
@@ -16,6 +53,17 @@ class Config {
     static STORE_LS_Config()
     {
         return '{"userId":0,"location":{"geohash":"ws0eegnkxc87","latitude":23.132495,"longitude":113.376836,"timestamp":' + (new Date()).getTime() + ',"locationName":"乐天大厦(西北门)"},"cityId":4,"districtId":5623}';
+    }
+
+    static login_Cookie_Config():Object
+    {
+        return {
+            name: "SID",
+            value: Config.getInstance().get_Conf_Value("SID"),
+            domain: ".ele.me",
+            path: "/",
+            httpOnly: true,
+            expires:new Date().getTime() / 1000 + 365 * 24 * 3600};
     }
 }
 export = Config;
